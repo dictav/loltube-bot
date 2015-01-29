0ad82962bb6d0d67486d5922e62be38d9a111a50
Shintaro Abe <dictav@gmail.com>
2015-01-29 20:06:33 +0900

    fix send parameters for facebook

--- lib/feed_post.rb
+++ lib/feed_post.rb
@@ -1,4 +1,5 @@
 require 'net/http'
+require 'uri'
 FB_PAGE_ID    = ENV['FB_PAGE_ID']
 FB_PAGE_TOKEN = ENV['FB_PAGE_TOKEN']
 
@@ -6,6 +7,7 @@ raise ArgumentError if FB_PAGE_ID.nil? or FB_PAGE_TOKEN.nil?
 
 def feed_post(message)
   raise ArgumentError unless message
+  link = URI.extract(message)
   url = URI("https://graph.facebook.com/v2.2/#{FB_PAGE_ID}/feed")
-  Net::HTTP.post_form url, {access_token: FB_PAGE_TOKEN, message: message}
+  Net::HTTP.post_form url, {access_token: FB_PAGE_TOKEN, message: message, link: link}
 end
